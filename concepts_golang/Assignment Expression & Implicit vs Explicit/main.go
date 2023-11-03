4e3765ab65b989969c1bd0a794e46f832a4d9960
himanshu <h.sahni1998@gmail.com>
2023-11-03 04:41:26 +0100

    commit

---
+++ concepts_golang/Assignment Expression & Implicit vs Explicit/main.go
@@ -0,0 +1,20 @@
+// we dont define the data type in this
+package main
+
+import "fmt" // display messages or text, print to the console
+
+func main() { // entrypoint
+
+	// var number = 2
+	// var number = 2000
+	// var number = 2000.98
+
+	number := 6 // expression assignment operator
+
+	fmt.Printf("%T", number)
+
+	// var number uint64		// Prints the default value (0 in this case)
+	// var bl bool
+	// fmt.Println(number, bl)
+
+}
